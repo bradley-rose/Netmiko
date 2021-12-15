e718420e4c88988870204b7f8cf2a208ee05905c
Bradley Rose <91505167+bradley-rose@users.noreply.github.com>
2021-12-14 19:12:47 -0700

    Updated asa.py
    asa.py now queries all devices in a CSV labelled `asas.csv` so long as the target IP to be SSH'd to is identified by a column labelled "Management IP". The output result is printed out to the console.

--- asa.py
+++ asa.py
@@ -1,23 +1,28 @@
 # Connection Handler
 from netmiko import Netmiko
 from getpass import getpass
+import csv
 
-devicePassword = getpass()
+deviceUsername = getpass(prompt='Username: ')
+devicePassword = getpass(prompt='Password: ')
 
 device = {
-    'username':'cisco',
+    'username':deviceUsername,
     'password':devicePassword,
-    'device_type':'cisco_asa'
+    'secret':devicePassword,
+    'device_type':'cisco_asa',
+    'session_log': 'troubleshooting.txt'
 }
 
-devices = ('addressA','addressB','addressC')
-for x in devices:
-    device['host'] = x
-    print(device['host'])
-    print(devicePassword)
-
-
-""" connection = Netmiko(**device)
-connection.enable('cisco')
-print(connection.find_prompt())
-print(connection.send_command('show version | grep Serial')) """
+with open('asas.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    line_count=0
+    for row in reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count +=1
+        else:
+            device['host'] = row['Management IP']
+            connection = Netmiko(**device)
+            print(row)
+            print(connection.send_command('show version | grep Serial'))
